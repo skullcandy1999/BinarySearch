8d0a28457ad4428ad763e89a1ffa808877e5dad9
Ishita Chattopadhyay <ishita@Ishitas-MacBook-Air.local>
2024-10-21 21:46:23 +0530

    Updated code with time complexity of O(log N).

--- findPosition.cpp
+++ findPosition.cpp
@@ -1,23 +1,42 @@
-#Time Complexity: O(N)
+#Time Complexity: O(log N)
 
 class Solution {
 public:
+    vector<int> findPos(int left, int mid, int right, vector<int>& nums){
+        while(left<=mid){
+            if(nums[left]==nums[mid]){
+                break;
+            }
+            else{
+                left++;
+            }
+        }
+
+        while(right>=mid){
+            if(nums[right]==nums[mid]){
+                break;
+            }
+            else{
+                right--;
+            }
+        }
+
+        return {left, right};
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int start=-1, end=-1;
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i]==target){
-                start=i;
-                break;
+        int left=0, right=nums.size()-1, mid;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return findPos(left, mid, right, nums);
+            }
+            if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
             }
         }
-
-        for(int i=nums.size()-1; i>=0; i--){
-            if(nums[i]==target){
-                end=i;
-                break;
-            }
-        }
-
-        return {start, end};
+        return {-1, -1};
     }
 };
